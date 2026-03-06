eb3383cab56b45bfd6abd6299a033c865737a835
Abhinavsharma005 <sharmaabhinav1013@gmail.com>
2026-03-06 22:28:34 +0530

    updated

--- src/Components/SkillsDataCard.jsx
+++ src/Components/SkillsDataCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Card from './Card'; 
-import { LuCode, LuZap } from 'react-icons/lu'; 
+import Card from './Card';
+import { LuCode, LuZap } from 'react-icons/lu';
 import { FaRegClock } from "react-icons/fa";
 import { GoPeople } from "react-icons/go";
 
@@ -8,22 +8,26 @@ const skillsData = [
   {
     title: '10+',
     description: 'Projects Completed',
-    icon:  FaRegClock
+    icon: FaRegClock,
+    color: 'text-cyan-400'
   },
   {
     title: 'Hackathon & Open Source',
     description: 'Worked with open source, hackathon and ready to contribute to communities',
-    icon: GoPeople 
+    icon: GoPeople,
+    color: 'text-cyan-400'
   },
   {
     title: 'AI Integration',
     description: 'Soon..Building tools with OpenAI Gemini, and custom LLM implementations',
-    icon: LuZap
+    icon: LuZap,
+    color: 'text-cyan-400'
   },
   {
     title: 'Clean Code',
     description: 'Writing modular, testable, and maintainable software',
-    icon: LuCode
+    icon: LuCode,
+    color: 'text-cyan-400'
   },
 ];
 
@@ -31,11 +35,12 @@ const SkillsDataCard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 p-4">
       {skillsData.map((skill, index) => (
-        <Card 
+        <Card
           key={index}
           title={skill.title}
           description={skill.description}
           Icon={skill.icon}
+          // color={skill.color}
         />
       ))}
     </div>
